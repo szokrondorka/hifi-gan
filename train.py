ce4d1bd8f8924d6ee51642ecab0c178301fd32c7
szokrondorka <71253642+szokrondorka@users.noreply.github.com>
2022-10-28 20:42:43 +0200

    Update train.py

--- train.py
+++ train.py
@@ -236,7 +236,7 @@ def main():
     parser.add_argument('--input_validation_file', default='LJSpeech-1.1/validation.txt')
     parser.add_argument('--checkpoint_path', default='cp_hifigan')
     parser.add_argument('--config', default='')
-    parser.add_argument('--training_epochs', default=3100, type=int)
+    parser.add_argument('--training_epochs', default=3, type=int)
     parser.add_argument('--stdout_interval', default=5, type=int)
     parser.add_argument('--checkpoint_interval', default=5000, type=int)
     parser.add_argument('--summary_interval', default=100, type=int)
